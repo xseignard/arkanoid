970b13a9d6a4142f41444b6d4a44307e1a1c3cbc
Martin Taibr <taibr.martin@gmail.com>
2015-04-17 20:39:16 +0200

    Brick collisions, "victory" condition

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ var WIDTH = 600; HEIGHT = 600;
 var States = {
 	STARTING: 1,
 	PLAYING: 2,
-	OVER: 3
+	OVERTIME: 3,
+	OVER: 4
 }
 var gameState = States.STARTING;
 
@@ -33,6 +34,7 @@ function init() {
 			bricks.push(new Brick(x, y));
 		}
 	}
+	//bricks.push(new Brick(100, 100));
 	window.setInterval(loop, 17);
 }
 
@@ -44,7 +46,7 @@ function start() {
 
 function loop() {
 	input();
-	if (gameState == States.PLAYING) logic();
+	logic();
 	draw();
 }
 
@@ -59,14 +61,16 @@ function input() {
 }
 
 function logic() {
+	if (gameState == States.OVER) return;
+
 	// move
 	ball.x += ball.speedX;
 	ball.y += ball.speedY;
 
 	// walls
-	if (ball.x - 2*Ball.radius < 0) ball.speedX = -ball.speedX;
-	if (ball.x + 2*Ball.radius > WIDTH) ball.speedX = -ball.speedX;
-	if (ball.y - 2*Ball.radius < 0) ball.speedY = -ball.speedY;
+	if (ball.x - 2*Ball.radius < 0 && ball.speedX < 0) ball.speedX = -ball.speedX;
+	if (ball.x + 2*Ball.radius > WIDTH && ball.speedX > 0) ball.speedX = -ball.speedX;
+	if (ball.y - 2*Ball.radius < 0 && ball.speedY < 0) ball.speedY = -ball.speedY;
 
 	// paddle
 	if (ball.y + Ball.radius + ball.speedY > paddle.y &&
@@ -90,12 +94,20 @@ function logic() {
 	// bricks
 	edgeX = ball.x + Ball.radius * Math.sign(ball.speedX);
 	edgeY = ball.y + Ball.radius * Math.sign(ball.speedY);
-	var verticalHit = false;
-	var horizontalHit = false;
 	for (i = 0; i < bricks.length; i++) {
-		//if (ball.x + Ball.radius >= brick.x && ball.x - Ball.radius <= brick.x + brick.w) {
-			//console.log(i);
-		//}
+		var brick = bricks[i];
+
+		// hit?
+		if (edgeX >= brick.x && edgeX <= brick.x + brick.w &&
+				edgeY >= brick.y && edgeY <= brick.y + brick.h) {
+			if (ball.x < brick.x || ball.x < brick.x + brick.w) ball.speedX = -ball.speedX;
+			if (ball.y < brick.y || ball.y < brick.y + brick.h) ball.speedY = -ball.speedY;
+			bricks.splice(i, 1);
+
+			if (bricks.length == 0) {
+				gameState = States.OVERTIME;
+			}
+		}
 	}
 }
 
@@ -111,17 +123,25 @@ function draw() {
 	c.stroke();
 
 	paddle.draw(c);
+
 	for (i = 0; i < bricks.length; i++) {
 		bricks[i].draw(c);
 	}
-	if (gameState != States.OVER) ball.draw(c);
-	else {
-		text = "Game Over";
-		c.font = '50px monospace';
-		c.fillStyle = 'black';
-		width = c.measureText(text).width;
-		c.fillText(text, WIDTH/2 - width/2, HEIGHT/2);
+
+	if (gameState == States.OVERTIME) {
+		centerText("You're not supposed to win", 30, HEIGHT/2 - 20);
+		centerText("Keep going", 30, HEIGHT/2 + 20);
 	}
+
+	if (gameState != States.OVER) ball.draw(c);
+	else centerText("Game Over", 50, HEIGHT / 2);
+}
+
+function centerText(text, size, height) {
+	c.font = size + 'px monospace';
+	c.fillStyle = 'black';
+	width = c.measureText(text).width;
+	c.fillText(text, WIDTH/2 - width/2, height);
 }
 
 function keydown(e) {
